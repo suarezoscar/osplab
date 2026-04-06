30d10956eb79249bf5f8c4780681fb78b63e2302
Oscar Suarez <osuarez@knowmadmood.com>
2026-04-06 16:24:08 +0200

    feat: implement new Privacy component and enhance home layout with accessibility improvements

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
-export const appRoutes: Route[] = [{ path: '', component: HomeComponent }];
+export const appRoutes: Route[] = [
+  { path: '', component: HomeComponent },
+  { path: 'privacidad', component: PrivacyComponent },
+];
